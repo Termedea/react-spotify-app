5a9c01ac34bf5096bb13471b27cff42d7259ec64
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2020-12-01 20:44:52 +0100

    init and tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['Texturina', 'ui-serif', 'serif'],
+                body: ['Nunito', 'ui-sans-serif'],
+                sans: ['Nunito', 'ui-sans-serif']
+            },
+            colors: {
+                primary: {
+                    lighter: '#f2f7fe',
+                    light: '#b2d0f9',
+                    DEFAULT: '#7fb1f6',
+                    dark: '#4c6a93',
+                    darker: '#192331'
+                },
+                secondary: {
+                    lighter: '#fdf3e5',
+                    light: '#f9dbb2',
+                    DEFAULT: '#f6c47f',
+                    dark: '#93754c',
+                    darker: '#493a26'
+                },
+                'acc-green': {
+                    lighter: '#e5fde7',
+                    light: '#cbfbcf',
+                    DEFAULT: '#7ff688',
+                    dark: '#3f7b44',
+                    darker: '#19311b'
+                },
+                'acc-pink': {
+                    lighter: '#fde5fb',
+                    light: '#fabff6',
+                    DEFAULT: '#f67fed',
+                    dark: '#7b3f76',
+                    darker: '#31192f'
+                }
+            }
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+};
